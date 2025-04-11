9f09241d8329aba5b590b8239f6f7553c029733e
britojp <joaopedrodebritotome@discente.ufg.br>
2025-04-11 13:58:05 -0300

    fix: correção registro de novos usuários

--- frontend/gerencia-supermercado/src/components/Table.vue
+++ frontend/gerencia-supermercado/src/components/Table.vue
@@ -1,52 +1,48 @@
 <template>
-    <v-table
-      height="300px"
-      fixed-header
+<v-data-table-virtual
+  :headers="headers"
+  :items="users"
+  height="100%"
+  item-value="name"
+  fixed-header
+>
+  <template #item.password="{ item }"
+  >
+      <span>{{ item.password }}</span>
+      <v-icon 
+      class="pl-5"
       >
-      <thead>
-        <tr>
-          <th class="text-center">
-            Nome Funcionário
-          </th>
-          <th class="text-center">
-            Email
-          </th>
-          <th class="text-center">
-            Senha
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in desserts"
-          :key="item.name"
-        >
-          <td>{{ item.name }}</td>
-          <td>{{ item.calories }}</td>
-        </tr>
-      </tbody>
-    </v-table>
+       {{item.icon}}
+       </v-icon>
   </template>
-  <script lag=ts >
-    export default {
-    name: 'Table',
-    components: {},
-    
-    props: {
-      user: {
+</v-data-table-virtual>
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Table',
+  components: {},
+  
+  props: {
+    headers: {
       type: Array,
       required: true,
-      default: () => [
-        {
-        name: 'Nome Funcionário',
-        email: 'Email',
-        password: 'Senha'
-        }
-      ]
-      }
     },
-
-    data() {
+    users: {
+      type: Array,
+      required: true
     }
-}
-  </script>
+  },
+
+  data() {
+  return {
+    show: false,
+  };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/gerencia-supermercado/src/pages/allEmployees.vue
+++ frontend/gerencia-supermercado/src/pages/allEmployees.vue
@@ -11,7 +11,7 @@
       
         <v-divider></v-divider>
 
-        <Table></Table>
+        <Table :headers="headers" :users="funcionarios"></Table>
   
   </v-card>
     </v-sheet>
@@ -28,7 +28,84 @@ import Table from '@/components/Table.vue';
   
     data () {
       return {
-  
+
+    headers : [
+    { title: 'Nome do Funcionário', align: 'center', key: 'name'},
+    { title: 'E-mail', align: 'center', key: 'email' },
+    { title: 'Senha', align: 'center', key: 'password'},
+  ],
+    funcionarios :[
+  {
+    name: 'Ana Souza',
+    email: 'ana.souza@example.com',
+    password: 'senha123',
+    icon: 'mdi-eye'
+  },
+  {
+    name: 'Carlos Lima',
+    email: 'carlos.lima@example.com',
+    password: '123senha',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'Fernanda Oliveira',
+    email: 'fernanda.oliveira@example.com',
+    password: 'minhasenha',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'João Pedro',
+    email: 'joao.pedro@example.com',
+    password: 'joao1234',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'Mariana Costa',
+    email: 'mariana.costa@example.com',
+    password: 'cost@123',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'Rafael Martins',
+    email: 'rafael.martins@example.com',
+    password: 'martins@2024',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'Luciana Ferreira',
+    email: 'luciana.ferreira@example.com',
+    password: 'lucy9876',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'Bruno Rocha',
+    email: 'bruno.rocha@example.com',
+    password: 'brunor!2023',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'Patrícia Mendes',
+    email: 'patricia.mendes@example.com',
+    password: 'pat123mendes',
+    icon: 'mdi-eye'
+
+  },
+  {
+    name: 'Thiago Barbosa',
+    email: 'thiago.barbosa@example.com',
+    password: 'tbarbosa!',
+    icon: 'mdi-eye'
+
+  }
+]
+
     }
     },
   
