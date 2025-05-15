eb7f95313047518b5611d9251261e4c36e7d83bc
britojp <joaopedrodebritotome@discente.ufg.br>
2025-05-15 15:18:32 -0300

    feat: finalizando autenticação

--- backend/gerencia-supermercado/src/app.module.ts
+++ backend/gerencia-supermercado/src/app.module.ts
@@ -6,11 +6,18 @@ import { ProductModule } from './product/product.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { SupplierModule } from './supplier/supplier.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 
 @Module({
   imports: [UsuarioModule, DatabaseModule, ProductModule, TransactionModule, SupplierModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
